5bb7d263fb9d5a95db6b6cd54aa5a3ebf65303a9
ahmed <a.t.elgammal@gmail.com>
2025-12-16 15:32:08 +0400

    add existing check for all tables for bronze layer

--- sql-warehouse-project.session.sql
+++ sql-warehouse-project.session.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS bronze_layer.crm_customer_info;
 CREATE TABLE bronze_layer.crm_customer_info(
     customer_id INT,
     customer_key VARCHAR(50),
@@ -6,13 +7,14 @@ CREATE TABLE bronze_layer.crm_customer_info(
     customer_material_status VARCHAR(50),
     customer_gender VARCHAR(50),
     customer_create_date DATE
-)
-ALTER TABLE bronze_layer.crm_customer_info
-DROP COLUMN customer_material_status;
+);
+-- ALTER TABLE bronze_layer.crm_customer_info
+-- DROP COLUMN customer_material_status;
 
-ALTER TABLE bronze_layer.crm_customer_info
-ADD COLUMN customer_marital_status VARCHAR(50);
+-- ALTER TABLE bronze_layer.crm_customer_info
+-- ADD COLUMN customer_marital_status VARCHAR(50);
 
+DROP TABLE IF EXISTS bronze_layer.crm_product_info;
 CREATE TABLE bronze_layer.crm_product_info(
     product_id INT,
     product_key VARCHAR(50),
@@ -23,6 +25,7 @@ CREATE TABLE bronze_layer.crm_product_info(
     product_end_date DATE
 );
 
+DROP TABLE IF EXISTS bronze_layer.crm_sales_info;
 CREATE TABLE bronze_layer.crm_sales_info(
     sales_order_number VARCHAR(50),
     sales_product_key VARCHAR(50),
@@ -35,17 +38,20 @@ CREATE TABLE bronze_layer.crm_sales_info(
     sales_price INT
 );
 
-CREATE TABLE bronze_layer.erb_customer_az12(
+DROP TABLE IF EXISTS bronze_layer.erb_location_a101;
+CREATE TABLE bronze_layer.erb_location_a101(
     customer_id VARCHAR(50),
     birth_date DATE,
     gender VARCHAR(20)
 );
 
+DROP TABLE IF EXISTS bronze_layer.erb_location_a101;
 CREATE TABLE bronze_layer.erb_location_a101(
     customer_id VARCHAR(50),
     country VARCHAR(50)
-)
+);
 
+DROP TABLE IF EXISTS bronze_layer.erb_category_glv2;
 CREATE TABLE bronze_layer.erb_category_glv2(
     category_id VARCHAR(50),
     category VARCHAR(50),
