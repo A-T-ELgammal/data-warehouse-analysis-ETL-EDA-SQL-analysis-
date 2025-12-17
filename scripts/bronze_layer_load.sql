8ca528e348b15a7b5313c3e8d98ec4965f2af08d
ahmed <a.t.elgammal@gmail.com>
2025-12-17 15:20:05 +0400

    try creating proceure and test its calling

--- scripts/bronze_layer_load.sql
+++ scripts/bronze_layer_load.sql
@@ -11,3 +11,30 @@ FROM bronze_layer.crm_customer_info;
 
 
 copy bronze_layer.crm_product_info(product_id, product_key, product_name, product_cost, product_line, product_start_date, product_end_date) FROM '/home/ahmed/projects/data_analysis/sql-data-warehouse-project-main/datasets/source_crm/prd_info.csv' WITH(FORMAT csv, DELIMITER ',', HEADER);
+
+-- testing creating procedure and call it.
+
+CREATE PROCEDURE silver_layer.silver_init()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+DROP TABLE IF EXISTS silver_layer.dumn;
+CREATE TABLE silver_layer.dumn(
+    id INT
+);
+
+END;
+$$;
+
+
+CALL silver_layer.silver_init();
+----
+
+CREATE PROCEDURE silver_layer.dumper()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+CREATE TABLE fir();
+END;
+$$;
