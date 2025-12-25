9d13f31d79d3986325e3d25118433cc94529a1a8
ahmed <a.t.elgammal@gmail.com>
2025-12-25 17:24:35 +0400

    add duration for data loading in each table

--- scripts/bronze_layer_load.sql
+++ scripts/bronze_layer_load.sql
@@ -6,7 +6,7 @@ start_time TIMESTAMP;
 patch_start_time TIMESTAMP;
 end_time TIMESTAMP;
 patch_end_time TIMESTAMP;
-
+duration INTERVAL;
 BEGIN
     RAISE NOTICE '=================================================';
     RAISE NOTICE 'Loading Bronze Layer';
@@ -14,16 +14,20 @@ BEGIN
 
     RAISE NOTICE 'Truncating table: bronze_layer.crm_customer_info';
     TRUNCATE TABLE bronze_layer.crm_customer_info;
-    start_time:= NOW();
+    start_time:= clock_timestamp();
     RAISE NOTICE 'Start loading data into the table: bronze_layer.crm_customer_info at // %', start_time;
-    end_time:= NOW();
+    end_time:= clock_timestamp();
+    duration:= end_time - start_time;
     BEGIN
         COPY bronze_layer.crm_customer_info FROM '/var/lib/postgresql/data/data-warehouse-project/crm_data/cust_info.csv' WITH CSV HEADER;
         RAISE NOTICE 'Loading data successfully at time:%', end_time; 
+        duration:= end_time - start_time;
+        RAISE NOTICE 'data loading took:  % ', duration;
     EXCEPTION
         WHEN OTHERS THEN
             RAISE NOTICE 'not successfully loading the data with that error: %', SQLERRM;
             RAISE NOTICE 'ending time is: % ', end_time;
+
     END;
 
     BEGIN
@@ -31,12 +35,14 @@ BEGIN
         RAISE NOTICE '=================================================';
         RAISE NOTICE 'Truncating table: bronze_layer.crm_product_info';
         TRUNCATE TABLE bronze_layer.crm_product_info;
-        start_time:= NOW();
+        start_time:= clock_timestamp();
         RAISE NOTICE 'Start loading data into the table: bronze_layer.crm_product_info at // %', start_time;
-        end_time:= NOW();
+        end_time:= clock_timestamp();
         BEGIN
             COPY bronze_layer.crm_product_info FROM '/var/lib/postgresql/data/data-warehouse-project/crm_data/prd_info.csv' WITH CSV HEADER;
             RAISE NOTICE 'Loading data successfully at time:%', end_time; 
+            duration:= end_time - start_time;
+            RAISE NOTICE 'data loading took:  % ', duration;
         EXCEPTION
             WHEN OTHERS THEN
                 RAISE NOTICE 'not successfully loading the data with that error: %', SQLERRM;
@@ -48,12 +54,14 @@ BEGIN
         RAISE NOTICE '=================================================';
         RAISE NOTICE 'Truncating table: bronze_layer.crm_sales_info';
         TRUNCATE TABLE bronze_layer.crm_sales_info;
-        start_time:= NOW();
+        start_time:= clock_timestamp();
         RAISE NOTICE 'Start loading data into the table: bronze_layer.crm_sales_info at // %', start_time;
-        end_time:= NOW();
+        end_time:= clock_timestamp();
         BEGIN
             COPY bronze_layer.crm_sales_info FROM '/var/lib/postgresql/data/data-warehouse-project/crm_data/sales_details.csv' WITH CSV HEADER;
-            RAISE NOTICE 'Loading data successfully at time:%', end_time; 
+            RAISE NOTICE 'Loading data successfully at time:%', end_time;
+            duration:= end_time - start_time;
+            RAISE NOTICE 'data loading took:  % ', duration;
         EXCEPTION
             WHEN OTHERS THEN
                 RAISE NOTICE 'not successfully loading the data with that error: %', SQLERRM;
@@ -65,12 +73,14 @@ BEGIN
         RAISE NOTICE '=================================================';
         RAISE NOTICE 'Truncating table: bronze_layer.erb_location_a101';
         TRUNCATE TABLE bronze_layer.erb_location_a101;
-        start_time:= NOW();
+        start_time:= clock_timestamp();
         RAISE NOTICE 'Start loading data into the table: bronze_layer.erb_location_a101 at // %', start_time;
-        end_time:= NOW();
+        end_time:= clock_timestamp();
         BEGIN
             COPY bronze_layer.erb_location_a101 FROM '/var/lib/postgresql/data/data-warehouse-project/erp_data/LOC_A101.csv' WITH CSV HEADER;
             RAISE NOTICE 'Loading data successfully at time:%', end_time; 
+            duration:= end_time - start_time;
+            RAISE NOTICE 'data loading took:  % ', duration;
         EXCEPTION
             WHEN OTHERS THEN
                 RAISE NOTICE 'not successfully loading the data with that error: %', SQLERRM;
@@ -82,12 +92,14 @@ BEGIN
         RAISE NOTICE '=================================================';
         RAISE NOTICE 'Truncating table: bronze_layer.erb_customer_az12';
         TRUNCATE TABLE bronze_layer.erb_customer_az12;
-        start_time:= NOW();
+        start_time:= clock_timestamp();
         RAISE NOTICE 'Start loading data into the table: bronze_layer.erb_customer_az12 at // %', start_time;
-        end_time:= NOW();
+        end_time:= clock_timestamp();
         BEGIN
             COPY bronze_layer.erb_customer_az12 FROM '/var/lib/postgresql/data/data-warehouse-project/erp_data/CUST_AZ12.csv' WITH CSV HEADER;
             RAISE NOTICE 'Loading data successfully at time:%', end_time; 
+            duration:= end_time - start_time;
+            RAISE NOTICE 'data loading took:  % ', duration;
         EXCEPTION
             WHEN OTHERS THEN
                 RAISE NOTICE 'not successfully loading the data with that error: %', SQLERRM;
@@ -99,19 +111,20 @@ BEGIN
         RAISE NOTICE '=================================================';
         RAISE NOTICE 'Truncating table: bronze_layer.erb_category_glv2';
         TRUNCATE TABLE bronze_layer.erb_category_glv2;
-        start_time:= NOW();
+        start_time:= clock_timestamp();
         RAISE NOTICE 'Start loading data into the table: bronze_layer.erb_category_glv2 at // %', start_time;
-        end_time:= NOW();
+        end_time:= clock_timestamp();
         BEGIN
             COPY bronze_layer.erb_category_glv2 FROM '/var/lib/postgresql/data/data-warehouse-project/erp_data/PX_CAT_G1V2.csv' WITH CSV HEADER;
             RAISE NOTICE 'Loading data successfully at time:%', end_time; 
+            duration:= end_time - start_time;
+            RAISE NOTICE 'data loading took:  % ', duration;
         EXCEPTION
             WHEN OTHERS THEN
                 RAISE NOTICE 'not successfully loading the data with that error: %', SQLERRM;
                 RAISE NOTICE 'ending time is: % ', end_time;
         END;
     END;
-
 END;
 $$;
 
@@ -122,13 +135,13 @@ CALL bronze_layer.bronze_load();
 
 --> Test
 
---> crm_tables:
-SELECT COUNT(*) FROM bronze_layer.crm_customer_info;
-SELECT COUNT(*) FROM bronze_layer.crm_product_info;
-SELECT COUNT(*) FROM bronze_layer.crm_sales_info;
+-- --> crm_tables:
+-- SELECT COUNT(*) FROM bronze_layer.crm_customer_info;
+-- SELECT COUNT(*) FROM bronze_layer.crm_product_info;
+-- SELECT COUNT(*) FROM bronze_layer.crm_sales_info;
 
 
---> erb_tables:
-SELECT COUNT(*) FROM bronze_layer.erb_customer_az12;
-SELECT COUNT(*) FROM bronze_layer.erb_category_glv2;
-SELECT COUNT(*) FROM bronze_layer.erb_location_a101;
+-- --> erb_tables:
+-- SELECT COUNT(*) FROM bronze_layer.erb_customer_az12;
+-- SELECT COUNT(*) FROM bronze_layer.erb_category_glv2;
+-- SELECT COUNT(*) FROM bronze_layer.erb_location_a101;
