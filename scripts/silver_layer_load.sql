696983cfe79e9978b9913516bdca2d975116a4dd
ahmed <a.t.elgammal@gmail.com>
2026-01-05 16:38:41 +0400

    inserting cleaned table into silver layer & test it

--- scripts/silver_layer_load.sql
+++ scripts/silver_layer_load.sql
@@ -1,21 +1,24 @@
 SELECT * From bronze_layer.crm_customer_info;
 
---> cleaning the crm_customer_info table
+--> cleaning the crm_customer_info table --- checking with no results
 
 SELECT customer_id, COUNT(*) 
-FROM bronze_layer.crm_customer_info
+FROM silver_layer.crm_customer_info
 GROUP BY customer_id
-HAVING COUNT(*) > 1 OR customer_id IS NULL;
+HAVING COUNT(*) > 1;
 
 
 ALTER TABLE bronze_layer.crm_customer_info 
 RENAME COLUMN customer_material_status TO customer_marital_status;
 
+ALTER TABLE silver_layer.crm_customer_info 
+RENAME COLUMN customer_material_status TO customer_marital_status;
+
 SELECT DISTINCT customer_marital_status
-FROM bronze_layer.crm_customer_info
+FROM silver_layer.crm_customer_info
 
 SELECT * 
-FROM bronze_layer.crm_customer_info
+FROM silver_layer.crm_customer_info
 WHERE customer_id = 29466;
 --> selecting just latest update of customer info 
 SELECT * 
@@ -25,25 +28,39 @@ SELECT *
 FROM bronze_layer.crm_customer_info
 )AS ranked_customers WHERE updated_state = 1;
 
---> customer_info cleaning query
+------------------------------------------------------------------
+--> customer info clean and insertion to silver layer
+
+INSERT INTO silver_layer.crm_customer_info(
+       customer_id,
+       customer_key,
+       customer_first_name,
+       customer_last_name,
+       customer_gender,
+       customer_marital_status,
+       customer_create_date
+)
+
+
+--> crm customer_info cleaning query -- final query
+with latest_customer_update AS (
+    SELECT *
+    , RANK() OVER (PARTITION BY customer_id ORDER BY customer_create_date DESC) AS updated_state
+    FROM bronze_layer.crm_customer_info)
 SELECT 
 customer_id, customer_key,
 TRIM(customer_first_name) AS customer_first_name,
 TRIM(customer_last_name) AS customer_last_name,
-TRIM(customer_gender) AS customer_gender,
-TRIM(customer_marital_status) AS customer_marital_status
-,CASE   WHEN UPPER(customer_gender) = 'M' THEN 'Male'
-        WHEN UPPER(customer_gender) = 'F' THEN 'Female'
+CASE   WHEN UPPER(TRIM(customer_gender)) = 'M' THEN 'Male'
+        WHEN UPPER(TRIM(customer_gender)) = 'F' THEN 'Female'
         ELSE 'Unknown'
- END customer_gender,
- CASE   WHEN UPPER(customer_marital_status) = 'S' THEN 'Single'
-        WHEN UPPER(customer_marital_status) = 'M' THEN 'Married'
+ END AS  customer_gender,
+ CASE   WHEN UPPER(TRIM(customer_marital_status)) = 'S' THEN 'Single'
+        WHEN UPPER(TRIM(customer_marital_status)) = 'M' THEN 'Married'
         ELSE 'Unkown'
- END customer_marital_status,
-customer_create_date
-FROM(
-SELECT *
-, RANK() OVER (PARTITION BY customer_id ORDER BY customer_create_date DESC) AS updated_state
-FROM bronze_layer.crm_customer_info
-)AS ranked_customers WHERE updated_state = 1;
+ END AS customer_marital_status ,
+ customer_create_date
+FROM latest_customer_update WHERE updated_state = 1;
 
+SELECT * FROM silver_layer.crm_customer_info;
+-------------------------------------------------------------
