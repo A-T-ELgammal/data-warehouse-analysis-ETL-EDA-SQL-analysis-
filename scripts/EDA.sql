21c4f31cd2fe962eea9efdb75098b4b774c08c1a
ahmed <a.t.elgammal@gmail.com>
2026-02-06 08:38:51 +0400

    date_range_exploration

--- scripts/EDA.sql
+++ scripts/EDA.sql
@@ -11,3 +11,25 @@ ORDER BY 1,2;
 
 SELECT DISTINCT product_line
 FROM gold_layer.dim_product_info;
+
+-------------------------------
+-- Date exploration
+
+SELECT 
+MIN(order_date) AS first_order_date,
+MAX(order_date) AS last_order_date
+FROM gold_layer.fact_sales_info;
+
+SELECT
+AGE(MAX(order_date), MIN(order_date)) AS orders_full_date_range
+FROM gold_layer.fact_sales_info;
+
+
+SELECT
+MIN(customer_birth_date) AS younger_customer,
+MAX(customer_birth_date) AS oldest_customer,
+AGE(MAX(customer_birth_date), MIN(customer_birth_date)) AS customer_age_range,
+AGE(NOW()::date, MIN(customer_birth_date)) AS youngest_customer_age,
+AGE(NOW()::date, MAX(customer_birth_date)) AS oldest_customer_age
+FROM gold_layer.dim_customer_info
+
