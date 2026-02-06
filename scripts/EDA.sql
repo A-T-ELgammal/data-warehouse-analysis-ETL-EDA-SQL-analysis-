3aa79d083e87ebadb84cb292dc0a86223c087a5a
ahmed <a.t.elgammal@gmail.com>
2026-02-06 09:01:50 +0400

    measure_exploration_queries

--- scripts/EDA.sql
+++ scripts/EDA.sql
@@ -33,3 +33,17 @@ AGE(NOW()::date, MIN(customer_birth_date)) AS youngest_customer_age,
 AGE(NOW()::date, MAX(customer_birth_date)) AS oldest_customer_age
 FROM gold_layer.dim_customer_info
 
+-------------------------------------------------------
+-- measure exploration
+
+SELECT * FROM gold_layer.fact_sales_info
+
+SELECT
+COUNT(DISTINCT order_number) AS total_orders,
+COUNT(DISTINCT customer_key) AS total_customer_place,
+COUNT(DISTINCT product_key) AS total_store_products,
+SUM(total_sales) AS sum_of_total_sales,
+ROUND(AVG(total_sales), 2) AS average_sales,
+SUM(quantity) AS total_sold_products,
+ROUND(AvG(price), 2) AS product_average_price
+FROM gold_layer.fact_sales_info
